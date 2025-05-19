519a9992f47455cd7f0e0dde73fc63f1bd20d17b
Ha Tran <hattt.1995@gmail.com>
2025-05-19 12:23:28 +0700

    Template

---
+++ app/mockdata.py
@@ -0,0 +1,18 @@
+user = {"username": "testuser", "age": "25", "email": "abc@def.com"}
+posts = [
+    {
+        "author": {"username": "user1"},
+        "title": "Post 1",
+        "content": "Content of post 1",
+    },
+    {
+        "author": {"username": "user2"},
+        "title": "Post 2",
+        "content": "Content of post 2",
+    },
+    {
+        "author": {"username": "user3"},
+        "title": "Post 3",
+        "content": "Content of post 3",
+    },
+]
